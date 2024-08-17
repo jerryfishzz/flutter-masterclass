e6dae47f6f4efe163a8ebc8c80a22d0be81e0bbf
Jerry Zhang <jerryfish@163.com>
2024-08-17 18:49:18 +1200

    #13 - Images
    feat: #13 - Images

--- lib/coffee_prefs.dart
+++ lib/coffee_prefs.dart
@@ -5,24 +5,34 @@ class CoffeePrefs extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
+    return Column(
       children: [
         Row(
           children: [
-            Text('Strength: '),
-            Text('3'),
-            SizedBox(
-              width: 50,
-            )
+            const Text('Strength: '),
+            const Text('3'),
+            Image.asset(
+              'assets/img/coffee_bean.png',
+              width: 25,
+              // Color blend mode to convert the image background color
+              // to the container background color
+              color: Colors.brown[100],
+              colorBlendMode: BlendMode.multiply,
+            ),
+            const SizedBox(width: 50),
           ],
         ),
         Row(
           children: [
-            Text('Sugar: '),
-            Text('3'),
-            SizedBox(
-              width: 50,
-            )
+            const Text('Sugar: '),
+            const Text('3'),
+            Image.asset(
+              'assets/img/sugar_cube.png',
+              width: 25,
+              color: Colors.brown[100],
+              colorBlendMode: BlendMode.multiply,
+            ),
+            const SizedBox(width: 50),
           ],
         ),
       ],
