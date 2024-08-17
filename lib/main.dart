b8ff31c40d3c644880784656fdb1c5504143e798
Jerry Zhang <jerryfish@163.com>
2024-08-17 14:50:52 +1200

    #9 containers
    feat: #9 containers

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -20,6 +21,27 @@ class Home extends StatelessWidget {
   // Hot reload needs build method
   @override
   Widget build(BuildContext context) {
-    return const Text('Hello, Jerry!!');
+    return Container(
+      color: Colors.orange,
+      // width: 200,
+      // height: 100,
+      padding: const EdgeInsets.all(20),
+      margin: const EdgeInsets.fromLTRB(10, 40, 0, 0),
+      child: const Text(
+        'Hello, Jerry',
+        style: TextStyle(
+            fontSize: 18,
+            letterSpacing: 4,
+            decoration: TextDecoration.underline,
+            fontStyle: FontStyle.italic),
+      ),
+    );
   }
 }
+
+// Container - wrap other widgets
+// Can apply padding and margin easily; can apply a color.
+// By default, container's width and heigh are decided by its children.
+// Can also specify the width and height.
+// If NO children, container will try to occupy the space as possible as it can.
+
