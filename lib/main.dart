e7ff3c51fca6b677d4790c8c20d477d7387b6031
Jerry Zhang <jerryfish@163.com>
2024-08-17 14:29:58 +1200

    #6 stateless widget
    feat: #6 stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,24 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
+      // Scaffold can make material styles display properly in the app
       home: Scaffold(
     appBar: AppBar(
       title: const Text('My Coffee Id'),
       backgroundColor: Colors.brown[700],
       centerTitle: true,
     ),
-    body: const Text('Hello, Jerry!'),
+    body: const Home(),
   )));
+}
+
+// Type 'STL' then choose to create a stateless widget
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  // Hot reload needs build method
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Hello, Jerry!!');
+  }
 }
