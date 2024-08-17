5e6f9ae64a07be195e7cafbfdfdde91794b8d4ba
Jerry Zhang <jerryfish@163.com>
2024-08-17 17:00:17 +1200

    #11 - Columns
    feat: #11 - Columns

--- lib/home.dart
+++ lib/home.dart
@@ -10,31 +10,26 @@ class Home extends StatelessWidget {
     // Scaffold can make material styles display properly in the app
     return Scaffold(
       appBar: AppBar(
-        title: const Text('My Coffee Id'),
+        title: const Text('My Coffee Id',
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
         backgroundColor: Colors.brown[700],
         centerTitle: true,
       ),
-      body: Container(
-        color: Colors.orange,
-        // width: 200,
-        // height: 100,
-        padding: const EdgeInsets.all(20),
-        margin: const EdgeInsets.fromLTRB(10, 40, 0, 0),
-        child: const Text(
-          'Hello, Jerry',
-          style: TextStyle(
-              fontSize: 18,
-              letterSpacing: 4,
-              decoration: TextDecoration.underline,
-              fontStyle: FontStyle.italic),
-        ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            color: Colors.brown[200],
+            padding: const EdgeInsets.all(20),
+            child: const Text('How I like my coffee...'),
+          ),
+          Container(
+            color: Colors.brown[100],
+            padding: const EdgeInsets.all(20),
+            child: const Text('Coffee prefs.'),
+          ),
+        ],
       ),
     );
   }
 }
-
-// Container - wrap other widgets
-// Can apply padding and margin easily; can apply a color.
-// By default, container's width and heigh are decided by its children.
-// Can also specify the width and height.
-// If NO children, container will try to occupy the space as possible as it can.
--- lib/main.dart
+++ lib/main.dart
@@ -6,3 +6,43 @@ void main() {
     home: Home(),
   ));
 }
+
+class Sandbox extends StatelessWidget {
+  const Sandbox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sandbox'),
+        backgroundColor: Colors.grey,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            width: 100,
+            color: Colors.red,
+            child: const Text('one'),
+          ),
+          Container(
+            width: 200,
+            color: Colors.green,
+            child: const Text('two'),
+          ),
+          Container(
+            width: 300,
+            color: Colors.blue,
+            child: const Text('three'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// 'Column' has 'children' field, but not 'child'.
+// Column width is dictated by the largest width item inside.
+// Use 'CrossAxisAlignment.stretch' will bypass the rule of largest width item.
+// Instead, try to occupy the space from its parent as passible as it can. 
